3623c554db30b31ef56216c9435810aa5c156ca8
R_XIAN <asdzxccxz333@gmail.com>
2021-02-17 22:35:41 +0800

    完成Google登入取資料

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,52 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <button type="button" id="btnSignIn" @click="login">Google登入</button>
+    <button type="button" id="btnDisconnect" @click="disconnect">
+      斷連Google App
+    </button>
+    <hr />
+    <div id="content"></div>
   </div>
 </template>
 
+<script src="https://apis.google.com/js/api.js"></script>
 <script>
 export default {
-  name: 'App'
-}
+  methods: {
+    login() {
+      this.$gapi.getGapiClient().then((gapi) => {
+        let auth2 = gapi.auth2.getAuthInstance(); // 取得GoogleAuth物件
+        auth2.signIn().then(function (GoogleUser) {
+          console.log("Google登入成功");
+          let AuthResponse = GoogleUser.getAuthResponse(true); // true會回傳包含access token ，false則不會
+          let id_token = AuthResponse.id_token; // 取得id_token
+          gapi.client.people.people
+            .get({
+              resourceName: "people/me",
+              personFields:
+                "names,phoneNumbers,emailAddresses,addresses,residences,genders,birthdays",
+            })
+            .then(function (res) {
+              let str = JSON.stringify(res.result); // 將物件列化成string，方便顯示結果在畫面上
+              document.getElementById("content").innerHTML = str;
+            });
+        });
+      });
+    },
+    disconnect() {
+      let auth2 = gapi.auth2.getAuthInstance(); // 取得GoogleAuth物件
+      auth2.disconnect().then(function () {
+        console.log("已斷連");
+        document.getElementById("content").innerHTML = "";
+      });
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import VueGapi from 'vue-gapi'
 Vue.config.productionTip = false
-
+Vue.use(VueGapi, {
+  clientId:
+    '238605304260-m6fi213b862ujp8n06ijj3kft1olu9sd.apps.googleusercontent.com',
+  discoveryDocs: ['https://people.googleapis.com/$discovery/rest'],
+  scope: 'profile'
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
