c2d1a57b6f6e8743a7fa8bfbea291ede00996ff1
R_XIAN <asdzxccxz333@gmail.com>
2021-05-03 22:27:15 +0800

    背景初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,5 +9,13 @@
 </script>
 
 <style>
-
+#home {
+  background-image: url("../../static/background.jpg");
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
 </style>
